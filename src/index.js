8ec53d12ffa14cc039f2429352f69d66c1a7cc00
Loyal Chow <loyalchow@gmail.com>
2017-01-14 16:34:47 -0800

    fix storybook object returning with extra properties

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 var Promise = require('bluebird');
+var pick = require('lodash/pick');
 
 exports.staticStorybook = Promise.promisify(require('./storybook/static'));
 
 exports.getStorybook = function() {
-  return require('./storybook');
+  var storybook = require('./storybook');
+  // make copy and remove methods
+  storybook = JSON.parse(JSON.stringify(storybook));
+  if (storybook instanceof Array) {
+    // remove any extra properties
+    storybook = storybook.map(function(kind) {
+      var obj = pick(kind, ['kind', 'stories']);
+      obj.stories = obj.stories.map(function(story) {
+        return pick(story, ['name']);
+      });
+      return obj;
+    });
+  }
+  return storybook;
 };
 
 exports.run = require('./runner').run;
