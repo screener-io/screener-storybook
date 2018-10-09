96ee9842aaea4761b7a487540e3eb303fe412a80
Loyal Chow <loyalchow@gmail.com>
2018-10-08 18:23:28 -0700

    add typescript declaration file

---
+++ index.d.js
@@ -0,0 +1,49 @@
+declare module 'screener-storybook/src/screener' {
+  export type Key =
+    | 'alt'
+    | 'control'
+    | 'enter'
+    | 'escape'
+    | 'return'
+    | 'shift'
+    | 'tab'
+    | 'leftArrow'
+    | 'upArrow'
+    | 'rightArrow'
+    | 'downArrow';
+
+  export const Keys: Record<Key, string>;
+
+  export interface SnapshotOpts {
+    cropTo: string
+  }
+
+  export interface Steps {
+    url(url: string): Steps;
+    click(selector: string): Steps;
+    cssAnimations(enabled: boolean): Steps;
+    snapshot(name: string, options?: SnapshotOpts): Steps;
+    focus(selector: string): Steps;
+    hover(selector: string): Steps;
+    mouseDown(selector: string): Steps;
+    mouseUp(selector: string): Steps;
+    keys(selector: string, keys: string): Steps;
+    setValue(selector: string, value: string): Steps;
+    executeScript(code: string, isAsync?: boolean): Steps;
+    ignore(selector: string): Steps;
+    wait(val: number | string): Steps;
+    rtl(): Steps;
+    ltr(): Steps;
+    end(): object[];
+  }
+
+  // tslint:disable-next-line
+  export class Steps implements Steps {}
+
+  export interface ScreenerProps {
+    steps?: Steps;
+    isScreenerComponent: boolean;
+  }
+
+  export default class Screener extends React.Component<ScreenerProps> {}
+}
