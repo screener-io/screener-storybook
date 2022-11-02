b88add0403435db94c8f70f0383f896c978c7374
Roland Hordos <roland.hordos@saucelabs.com>
2022-11-02 19:07:03 +0000

    VSLT-3170 - Node 16 NPM 8 + Obsolete CI Prune

--- test/e2e/screener.static.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  projectRepo: 'screener-storybook/tests/e2e/static',
-  storybookConfigDir: '.storybook',
-  apiKey: process.env.SCREENER_API_KEY,
-  resolution: '1024x768',
-  storybookStaticBuildDir: 'storybook-static'
-};
