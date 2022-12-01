bd6425ae69e7a3de267fd382cb777e89e36d233d
Gabriel Wady Debes Pacheco <gabriel.debes@saucelabs.com>
2022-12-01 00:02:50 +0000

    VSLT-3222 Deprecate `experimentalHook` flag in favor of new storybook automatically version check

--- src/features.js
+++ src/features.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const fs = require('fs');
+var child = require('child_process');
+var semver = require('semver');
 
 //  Return a list of features per [storybook doc](https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#using-the-v7-store)
 //  This is configured within the site under test main.js
@@ -41,6 +43,15 @@ const getStorybookFeatures = function() {
     console.info('screener-storybook Found framework', frameworkUnderTest, 'configured in main.js');
   }
 
+  const usesFeaturedServer = (
+    (storybookMain.features && storybookMain.features.storyStoreV7) || //Automatically fallback to stories align
+    isStorybookFeaturedServer()
+  );
+  if (!usesFeaturedServer) {
+    console.info('screener-storybook No featured server detected, using legacy path');
+    return {};
+  }
+
   const features = storybookMain.features;
   return {
     framework: frameworkUnderTest,
@@ -55,7 +66,39 @@ const isEmpty = function isEmpty(obj) {
   return !Object.keys(obj).length;
 };
 
+const getStorybookVersion = function getStorybookVersion() {
+  const storybookVersionRegex = /(@storybook\/[^@]+)@(\S+)/;
+  try {
+    let lines = child.execSync('npm ls').toString('utf8');
+    //remove initial line
+    if (lines && lines.length > 0) {
+      lines = lines.split('\n');
+      lines.shift();
+    }
+    //attempt to find storybook
+    const storybookPackage = lines.find((line) => {
+      const match = storybookVersionRegex.exec(line);
+      return (match && semver.clean(match[2]));
+    });
+    //Found storybook dep?
+    if (!storybookPackage) return null;
+    const match = storybookVersionRegex.exec(storybookPackage);
+    //Return version if valid
+    if (match[2]) return match[2];
+  } catch (e) {
+    console.info('screener-storybook Exception while attempting to retrieve storybook dependecy version', e);
+  }
+  return null;
+};
+
+const isStorybookFeaturedServer = function () {
+  const storybookVersion = getStorybookVersion();
+  return (storybookVersion && semver.gt(storybookVersion, '6.4.0'));
+};
+
 module.exports = {
   getStorybookFeatures,
   isEmpty,
+  getStorybookVersion,
+  isStorybookFeaturedServer
 };
